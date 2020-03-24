74eff45d6c7f6f40b89e6742866bb95698d78e25
Polanco <cpolanco@homework.cs.uri.edu>
2020-03-24 18:25:40 -0400

    Bleh

---
+++ fileOps.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "assert.h"
+#include "uarray2.h" 
+
+UArray2_T readFile(FILE * fp){
+    FILE* fp = fopen(argv[i], "r");
+    assert(fp);
+
+    //read in ppm image and fill a UArray2
+}
+
+void writeFile(UArray2_T uarray2){
+    //use a 2d loop and write to a file
+}
