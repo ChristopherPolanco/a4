fa7a0442d6077533c05b0fc23d87405f784f4f6e
jabouch98 <33700269+jabouch98@users.noreply.github.com>
2020-03-28 17:05:31 -0400

    Create componentBit.h

---
+++ componentBit.h
@@ -0,0 +1,15 @@
+#ifndef COMPONENTBIT_INCLUDED
+#define COMPONENTBIT_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "bitpack.h"
+#include "componentBit.h"
+#include "a2methods.h"
+#include "uarray2.h"
+#include "assert.h"
+
+extern void componentToBit(A2Methods_UArray2 array);
+
+#undef T
+#endif
