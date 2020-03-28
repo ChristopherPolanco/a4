4260dd5aa44ddc752cd22b603a0c0c49de6de788
Polanco <cpolanco@homework.cs.uri.edu>
2020-03-28 17:01:22 -0400

    finished bitpack and compile

--- bitpack.c
+++ bitpack.c
@@ -8,7 +8,7 @@ Except_T Bitpack_Overflow = {"Overflow packing bits"};
 
 bool Bitpack_fitsu(uint64_t n, unsigned width){
     //check width is in bounds
-    assert(width <= 64 && width >= 0);
+    assert(width <= 64 && width > 0);
 
     //check if width can accomadate n
     if((pow(2, width) - 1) < n){
@@ -19,7 +19,7 @@ bool Bitpack_fitsu(uint64_t n, unsigned width){
 }
 
 bool Bitpack_fitss( int64_t n, unsigned width){
-    assert(width <= 64 && width >= 0);
+    assert(width <= 64 && width > 0);
 
     //check if width can accomadate n
 	if (n >= (- pow(2, width)) && n <= (pow(2, width) - 1)){
@@ -32,6 +32,7 @@ bool Bitpack_fitss( int64_t n, unsigned width){
 uint64_t Bitpack_getu(uint64_t word, unsigned width, unsigned lsb){
     assert((width + lsb) <= 64);
 
+//Shift bits on both sides
     word = word >> (64 - width);
     word = word << (64 - (width + lsb));
     return word;
@@ -42,15 +43,31 @@ int64_t Bitpack_gets(uint64_t word, unsigned width, unsigned lsb){
 
     int64_t value = word >> (64 - width);
     value = word << (64 - (width + lsb));
-    return word;
+    return value;
 }
 
 uint64_t Bitpack_newu(uint64_t word, unsigned width, unsigned lsb, uint64_t value){
-    
+    assert(width + lsb <= 64 && width <= 64);
+
+   if(Bitpack_fitsu(value, width) == false){
+       RAISE(Bitpack_Overflow); 
+   }
+
+   uint64_t var = pow(2, width) - 1;
+   var = ~(var << lsb);
+   word = (word & var) | (value << (lsb));
+   return word;
 }
 
 uint64_t Bitpack_news(uint64_t word, unsigned width, unsigned lsb,  int64_t value){
+    assert(width + lsb <= 64 && width <= 64);
+    
+    uint64_t var = pow(2, width) - 1;
 
+   if(Bitpack_fitss(value, width) == false){
+        RAISE(Bitpack_Overflow); 
+   }
+
+   value = value & var;    
+   return (Bitpack_newu(word, width, lsb, value));
 }
-
-extern Except_T Bitpack_Overflow;
